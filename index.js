b59a9205b77f6606e417f6490f93392c79d53f80
Melissa <mwstraus@gmail.com>
2019-01-21 21:36:14 -0500

    Added JS to password input

---
+++ index.js
@@ -0,0 +1,39 @@
+//variables
+
+var password = "";
+var enterButton = document.getElementsByClassName('enter')[0];
+var errorMessages = document.getElementsByClassName('errorMessage');
+var pass = document.getElementById('pass');
+
+//functions
+
+function checkPassword(){
+
+  password = pass.value;
+  if (password == 'charlierulez'){
+    logIn();
+  }
+  else {
+    displayError();
+  }
+}
+
+//when user clicks `let me in`, check that they have entered `charlierulez`
+
+enterButton.addEventListener("click", checkPassword);
+pass.addEventListener("keypress", function(event){
+  if (event.key == "Enter"){
+    checkPassword();
+  }
+});
+
+function logIn(){
+  location = "https://rayandmelissa.com/home.html";
+}
+
+function displayError(){
+  errorMessages[0].className = errorMessages[0].className + ' errorMessageDisplay';
+  errorMessages[1].className = errorMessages[1].className + ' errorMessageDisplay';
+}
+
+//if user has entered password in the past, remember this
